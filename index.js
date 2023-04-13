dbc272357c2e48205d630970d65b65ae3bd85ef7
Lakshya000007 <lakshya719@gmail.com>
2023-04-13 15:45:24 +0530

    Async and await

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-let posts = [];
-let updateTime;
+//Change to async await
+async function print() {
+  let posts = [];
+  let updateTime;
 
-function updateLastUserActivityTime() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      updateTime = Date.now();
-      resolve(updateTime);
-    }, 1000);
-  });
+  function updateLastUserActivityTime() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        updateTime = Date.now();
+        resolve(updateTime);
+      }, 1000);
+    });
+  }
+
+  function createPost(post) {
+    return new Promise((reslove, reject) => {
+      setTimeout(() => {
+        posts.push(post);
+        updateLastUserActivityTime();
+        reslove(posts);
+      }, 1000);
+    });
+  }
+
+  createPost("Aman");
+  createPost("Twinkle");
+  const data = await Promise.all([
+    createPost("Lakshya"),
+    updateLastUserActivityTime(),
+  ]);
+  console.log(data);
+  createPost("Aanchal");
+  const newData = await Promise.all([
+    createPost("Lakshya"),
+    updateLastUserActivityTime(),
+  ]);
+  console.log(newData);
 }
 
-function createPost(post) {
-  return new Promise((reslove, reject) => {
-    posts.push(post);
-    updateLastUserActivityTime();
-    reslove(posts);
-  });
-}
-
-function getDetails() {
-  Promise.all([createPost("Lakshya"), updateLastUserActivityTime()]).then(
-    (data) => {
-      console.log(data);
-    }
-  );
-}
-
-createPost("Aman");
-
-getDetails();
-
-createPost("Twinkle");
+print();
